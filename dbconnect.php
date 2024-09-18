d1c35cc8c3e59d181a503d112cef82ab97add0bd
Noob-Ovi <abdullah.ovi098@gmail.com>
2024-09-18 09:23:05 +0600

    Added insertion feature

--- dbconnect.php
+++ dbconnect.php
@@ -4,16 +4,18 @@ $db_user = "root";
 $db_pass="";
 $db_name="phpcrud";
 $db_port= 3307;
-$conn ="";
+$conn =null;
 
 try{
-    $conn = new mysqli($db_host,$db_user,$db_pass,$db_name,$db_port);
+    if (!isset($conn)) {
+        $conn = new mysqli($db_host, $db_user, $db_pass, $db_name, $db_port);
+    }
     if($conn->connect_error){
         throw new Exception($conn->connect_error);
     }
-    else{
-        echo "Connected successfully";
-    }
+    // else{
+    //     echo "Connected successfully";
+    // }
 }catch(Exception $error){
     echo "Exception thrown in File:{$error->getFile()} on line:{$error->getLine()}";
     echo "Code: {$error->getCode()}";
--- index.php
+++ index.php
@@ -1,23 +1,74 @@
+<?php
+    include("dbconnect.php");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <div>
-        <label for=""> Name:</label>
-        <input type="text" name="name" id="">
-    </div>
-    <div>
-        <label for="">Phone:</label>
-        <input type="text" name="phone" id="">
-    </div>
-    <div>
-        <label for="">Image:</label>
-        <input type="file" name="phone" id="">
-    </div>
-    <input type="submit" name="submit" value="Submit">
+
+<?php
+   if($_SERVER['REQUEST_METHOD']==="POST"){
+    if(empty($_POST["name"])){
+        echo "Name is required";
+    }
+    else if(empty($_POST["phone"])){
+        echo "Phone number is required";
+    }
+    else if(empty($_FILES["photo"]["name"])){
+        echo "Photo is required";
+    }
+
+    else{
+        $name = $_POST["name"];
+        $phone =$_POST["phone"];
+        $image = $_FILES["photo"]["name"];
+        $dst = "./image/" .$image;
+        $dst_db = "image/" .$image;
+        
+        if(move_uploaded_file($_FILES["photo"]["tmp_name"],$dst)){
+            $query = "Insert INTO student (name, phone, image) VALUES('$name','$phone','$dst_db')";
+            try{
+                $result = mysqli_query($conn,$query);
+                if($result){
+                    echo "Information uploaded successful.";
+                }
+            }catch(mysqli_sql_exception $error){
+                echo "Error in file: {$error->getFile()} in line: {$error->getLine()}";
+                echo "Error: " . $error->getMessage();
+            }
+            finally{
+                $conn->close();
+            }
+        }
+        else{
+            echo "Something went wrong while uploading information";
+        }
+       
+    }
+   }
+?>
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post" enctype="multipart/form-data">
+        <div>
+            <label for=""> Name:</label>
+            <input required type="text" name="name" id="">
+        </div>
+        <div>
+            <label for="">Phone:</label>
+            <input required type="text" name="phone" id="">
+        </div>
+        <div>
+            <label for="">Image:</label>
+            <input required type="file" name="photo" id="">
+        </div>
+        <input type="submit" name="submit" value="Submit">
+    </form>
 </body>
+
 </html>
