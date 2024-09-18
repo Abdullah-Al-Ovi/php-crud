736daced68fe09bcada2515328566e04a0c4cb28
Noob-Ovi <abdullah.ovi098@gmail.com>
2024-09-18 07:11:21 +0600

    Project setup done

---
+++ dbconnect.php
@@ -0,0 +1,21 @@
+<?php
+$db_host = "localhost";
+$db_user = "root";
+$db_pass="";
+$db_name="phpcrud";
+$db_port= 3307;
+$conn ="";
+
+try{
+    $conn = new mysqli($db_host,$db_user,$db_pass,$db_name,$db_port);
+    if($conn->connect_error){
+        throw new Exception($conn->connect_error);
+    }
+    else{
+        echo "Connected successfully";
+    }
+}catch(Exception $error){
+    echo "Exception thrown in File:{$error->getFile()} on line:{$error->getLine()}";
+    echo "Code: {$error->getCode()}";
+    echo "Message:{$error->getMessage()}";
+}
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <label for=""> Name:</label>
+        <input type="text" name="name" id="">
+    </div>
+    <div>
+        <label for="">Phone:</label>
+        <input type="text" name="phone" id="">
+    </div>
+    <div>
+        <label for="">Image:</label>
+        <input type="file" name="phone" id="">
+    </div>
+    <input type="submit" name="submit" value="Submit">
+</body>
+</html>
